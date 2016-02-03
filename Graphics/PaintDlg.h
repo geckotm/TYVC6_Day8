0605818ed3657c3e6e929db90d4b397e479b5399
geckotm <geckotm@gmail.com>
2016-02-03 21:22:22 +0200

    second dialog

---
+++ Graphics/PaintDlg.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "afxdialogex.h"
+class CPaintDlg : public CDialogEx
+{
+// Construction
+public:
+	CPaintDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PAINT_DLG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+														// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
+
